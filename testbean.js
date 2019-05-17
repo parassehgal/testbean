8714827fb664b43f2957e0832267b749f469ca39
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-05-17 14:33:52 +0530

    Add files via upload

---
+++ testbean.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var app = express();
+app.use(
+		bodyParser.urlencoded(
+		{ extended: true })
+	);
+app.use(bodyParser.json());
+
+app.post('/testdialogflow',function (req, res) {
+	common.log('Inside /testdialogflow');
+	//var current_mode = "customise";
+	var ans;
+	var v=req.body.queryResult;
+	
+	if(v.intent.displayName == "on_off_state")
+	{	
+		ans = "Okay ac turned on";					
+	}
+	else 
+	{
+		ans = "Sorry i am in else"; 
+	}
+	
+	return res.json({
+				fulfillmentText: ans,
+				source: "testbean"
+			  });
+			  
+}).listen(process.env.PORT||9879);
