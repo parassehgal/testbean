bb42678aa1316589ebcb0c11a52ccb08ab54dbfe
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-05-21 15:27:21 +0530

    Add files via upload

--- testbean.js
+++ testbean.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var request = require('request');
 var app = express();
+var fs =require('fs');
 app.use(
 		bodyParser.urlencoded(
 		{ extended: true })
@@ -15,24 +16,62 @@ res.send('Inside getheroku');
 app.post('/testdialogflow',function (req, res) {
 	console.log('Inside /testdialogflow');
 	
-	  request.get('https://www.securesmarthome.co:1144/dialogflow',
-	   function(err,response,body){
-			if(!err && response.statusCode == 200)
-			{
-				//res.send(body);
-				res.json({
-						fulfillmentText: 'Sucess',
-						source: "testbean"
-					  });
+	
+	 request.post(
+	   {
+			url : 'https://www.securesmarthome.co:1144/dialogflow',
+			headers : {
+				authorization : req.headers['authorization']
+			},	
+			json : req.body,			
+			strictSSL: false
+		}, 
+	    function(err,response,b){
+			 if(err || response.statusCode != 200)
+			 {
+			
+				 res.json({
+						 fulfillmentText: 'error while calling api: ' +err+ ' , code: ' + ((typeof(response)=='undefined' ||  response==null)?' Undefined': response.statusCode) ,
+						 source: "testbean12"
+					   });
+				
+			 }
+		 else
+			 {
+			 res.json({
+						 fulfillmentText: response.body.fulfillmentText,
+						 source: response.body.source
+					   });
 			}
-			else
-			{
-				res.json({
-						fulfillmentText: 'error while calling api: ' +err+ ' , code: ' +  response.statusCode ,
-						source: "testbean"
-					  });
+		
+	    });
+	
+	
+	//www.securesmarthome.co
+	  /*request.get(
+	   {
+			url : 'https://172.16.231.81:1144/dialogflow',
+		 
+		 strictSSL: false
+		 }, 
+	    function(err,response,body){
+			 if(err || response.statusCode != 200)
+			 {
+			res.send(body);
+				 res.json({
+						 fulfillmentText: 'error while calling api: ' +err+ ' , code: ' + ((typeof(response)=='undefined' ||  response==null)?' Undefined': response.statusCode) ,
+						 source: "testbean12"
+					   });
+				
+			 }
+		 else
+			 {
+			 res.json({
+						 fulfillmentText: 'Success',
+						 source: "testbean1"
+					   });
 			}
-	   });
+	    });*/
 	
 	/*request.post('https://www.securesmarthome.co:1144/dialogflow',
 	{
@@ -56,23 +95,6 @@ app.post('/testdialogflow',function (req, res) {
 		}
 	});*/
 }).listen(process.env.PORT||9879);
+console.log('Server running');
 	
-	//var current_mode = "customise";
-	/*var ans;
-	var v=req.body.queryResult;
 	
-	if(v.intent.displayName == "on_off_state")
-	{	
-		ans = "Okay ac turned on";					
-	}
-	else 
-	{
-		ans = "Sorry i am in else"; 
-	}
-	
-	return res.json({
-				fulfillmentText: ans,
-				source: "testbean"
-			  });
-			  
-}).listen(process.env.PORT||9879);*/
