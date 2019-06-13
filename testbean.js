b8db6dab00e0dadfd6ca9f0314ce63be60f3cfb3
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-06-13 18:37:44 +0530

    Add files via upload

--- testbean.js
+++ testbean.js
@@ -102,7 +102,18 @@ app.post('/alexa',function(req,res){
 
 });
 
+app.post('/alexa/token',function(req,res){
 
+log('Inside heroku access token');
+		request.post(
+	    {
+			url : 'https://115.254.126.74:1144/oauth/alexatoken',	
+			json : req.body,			
+			strictSSL: false
+		}, 
+	    function(err,response,b){
+		});
+});
 
 app.listen(process.env.PORT||9879);
 console.log('Server running');
