062fcdc04bb89f8e73cb9ff0131979ee02f01083
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-06-17 12:06:46 +0530

    Add files via upload

--- testbean.js
+++ testbean.js
@@ -104,9 +104,9 @@ app.post('/alexa',function(req,res){
 
 app.post('/alexa/token',function(req,res){
 
-log('Inside heroku access token',true);
+/*log('Inside heroku access token',true);
 log(JSON.stringify(req.body),true);
-console.log(JSON.stringify(req.body));
+console.log(JSON.stringify(req.body));*/
 
 		request.post(
 	    {
