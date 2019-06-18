c874e23a1e7d785f6a9a8d72b53712e25a7abc0c
Paras Sehgal <34476449+parassehgal@users.noreply.github.com>
2019-06-18 11:57:56 +0530

    Add files via upload

--- testbean.js
+++ testbean.js
@@ -111,34 +111,40 @@ app.post('/alexa/token',function(req,res){
 
 //var rb = {"grant_type":"authorization_code","code":"136be4f9098b1f4aac6409086419e987e0c52d67f7ad42c314751982ba1ae94d1572a7bf17b405fe","redirect_uri":"https://layla.amazon.com/api/skill/link/M1HXPN4UNAI4NX","client_id":"LESEHejecNlArGo3lQ6GqZ62CaLRcP"};
 
+	try{
 
-		request.post(
-	    {
-			url : 'https://115.254.126.74:1144/oauth/alexatoken',	
-			headers: req.headers,
-			json : req.body,			
-			strictSSL: false
-		}, 
-	    function(err,response,b){
-			if(err || response.statusCode != 200)
-				{
-					res.json({
-							"access_token" : null,
-							"token_type" : null,
-							"expires_in" : null,
-							"refresh_token" : null
-						   });
-				}
-			 else
-				{
-				 res.json({
-							"access_token" : response.body.access_token,
-							"token_type" : "bearer",
-							"expires_in" : response.body.expires_in,
-							"refresh_token" : response.body.refresh_token
-						   });
-				}	
-		});
+			request.post(
+			{
+				url : 'https://115.254.126.74:1144/oauth/alexatoken',	
+				headers: req.headers,
+				json : JSON.parse(req.body),			
+				strictSSL: false
+			}, 
+			function(err,response,b){
+				if(err || response.statusCode != 200)
+					{
+						res.json({
+								"access_token" : null,
+								"token_type" : null,
+								"expires_in" : null,
+								"refresh_token" : null
+							   });
+					}
+				 else
+					{
+					 res.json({
+								"access_token" : response.body.access_token,
+								"token_type" : "bearer",
+								"expires_in" : response.body.expires_in,
+								"refresh_token" : response.body.refresh_token
+							   });
+					}	
+			});
+	}
+	catch(e)
+	{
+	log(e.stack,true);
+	}
 });
 
 app.listen(process.env.PORT||9879);
